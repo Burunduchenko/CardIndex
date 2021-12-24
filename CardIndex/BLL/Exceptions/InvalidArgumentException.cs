e97658ddd2f7d911dadfa9924ed3bd64afb12e19
Andrii Sova <sovaandriy95@gmail.com>
2021-12-25 00:11:44 +0200

    Swagger+Logs
    Was developed Swagger and Logs and version of project was downgradet to 5.0.1

---
+++ CardIndex/BLL/Exceptions/InvalidArgumentException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Exceptions
+{
+    public class InvalidArgumentException : Exception
+    {
+        public readonly string Massege = "Provided object data is invalid";
+    }
+}
---
+++ CardIndex/CardIndex/Controlers/ThemeController.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BLL.Interfaces;
+using BLL.Models;
+using BLL.Services;
+using BLL.Exceptions;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CardIndex.Controlers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ThemeController : ControllerBase
+    {
+        private readonly IService<ThemeModel> _themeService;
+
+        public ThemeController(IService<ThemeModel> themeService)
+        {
+            _themeService = themeService;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ThemeModel>> GetAll()
+        {
+            return Ok(_themeService.GetAllWithDetails());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ThemeModel>> GetById(int id)
+        {
+            try
+            {
+                var articleRateModel = await _themeService.GetByIdAsync(id);
+                return Ok(articleRateModel);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+
+        // POST api/<ArticleController>
+        ///
+        [HttpPost]
+        public async Task<IActionResult> Add([FromBody] ThemeModel articleModel)
+        {
+            try
+            {
+                var result = await _themeService.AddAsync(articleModel);
+                return Ok(result);
+            }
+            catch (InvalidArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (AlreadyExistException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+
+        // PUT api/<ArticleController>/5
+        [HttpPut]
+        public ActionResult<ThemeModel> Update([FromBody] ThemeModel value)
+        {
+            try
+            {
+                var result = _themeService.Update(value);
+                return Ok(result);
+            }
+            catch (InvalidArgumentException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+            catch (NotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+        }
+
+        // DELETE api/<ArticleController>/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteById(int id)
+        {
+            try
+            {
+                _themeService.Delete(id);
+                return Ok();
+
+            }
+            catch (NotFoundException ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
